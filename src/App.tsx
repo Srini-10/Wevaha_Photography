6dd94002182f053fe946a6391d493571cc6abc58
Srini-10 <srinivasanp1018@gmail.com>
2024-08-31 03:09:31 +0530

    Responsive Final Phase

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
-import Home from "./pages/Home.tsx";
-import About from "./pages/About.tsx";
-import Gallery from "./pages/Gallery.tsx";
-import Contact from "./pages/Contact.tsx";
 import LoadingAnimation from "./LoadingAnimation.tsx";
 import "./index.css";
+
+// Lazy loading your pages
+const Home = React.lazy(() => import("./pages/Home.tsx"));
+const About = React.lazy(() => import("./pages/About.tsx"));
+const Gallery = React.lazy(() => import("./pages/Gallery.tsx"));
+const Contact = React.lazy(() => import("./pages/Contact.tsx"));
 
 function App() {
   const [loading, setLoading] = useState(true);
   const location = useLocation();
 
   useEffect(() => {
+    // Display loading animation on location change
     setLoading(true);
 
     const timer = setTimeout(() => {
@@ -23,26 +26,19 @@ function App() {
 
   return (
     <>
-      <div className="unresponsive-view hidden w-full h-screen py-[20%] justify-center items-center bg-black">
-        <div className="text-center min-h-[20%] max-h-[20%]">
-          <h1 className="text-[150px] font-bold text-white">Oops!</h1>
-          <p className="text-[25px] text-neutral-600">
-            Your device is not compatible with this website.
-          </p>
-        </div>
-      </div>
-
-      <div className="normal-view">
+      <div className="">
         {loading ? (
           <LoadingAnimation />
         ) : (
           <div className="scroll-smooth focus:scroll-auto">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about_us" element={<About />} />
-              <Route path="/gallery" element={<Gallery />} />
-              <Route path="/contact_us" element={<Contact />} />
-            </Routes>
+            <Suspense fallback={<LoadingAnimation />}>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about_us" element={<About />} />
+                <Route path="/gallery" element={<Gallery />} />
+                <Route path="/contact_us" element={<Contact />} />
+              </Routes>
+            </Suspense>
           </div>
         )}
       </div>
